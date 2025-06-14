d46dba82d1b87932263cf50bb4c0de79bf137c53
sayyed135 <sayyedmohammadsadat10001@gmail.com>
2025-06-14 22:57:11 +0430

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-pyTelegramBotAPI
+import telebot
+
+# توکن رباتت رو اینجا قرار بده
+TOKEN = "7217912729:AAGsCp-YNxnlMUB8M352p2EcVGB2K0at2Xg"
+bot = telebot.TeleBot(TOKEN)
+
+# وقتی کاربر /start رو بفرسته
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "سلام! ربات من روشنه.")
+
+# اجرای ربات
+if __name__ == '__main__':
+    print("Bot started...")
+    bot.polling()
